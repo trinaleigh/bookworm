4efb45fece3cf76197f201af126a8ad7e6e0d476
trinaleigh <trinaleigh@users.noreply.github.com>
2017-02-06 16:29:29 -0500

    fix server config

--- src/server.js
+++ src/server.js
@@ -1,24 +1,14 @@
-import path from 'path';
-import { Server } from 'http';
-import Express from 'express';
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import { match, RouterContext } from 'react-router';
-import routes from './routes';
-import NotFound from './components/NotFound';
-
 var express = require('express');
 var app = express();
 var fetch = require('node-fetch');
 
-const server = new Server(app);
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
-
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/static'));
+
+app.get('/', function(request, response) {
+  response.render('static/index');
+});
 
 app.get('/books/:isbn', function(request, response) {
 
@@ -34,29 +24,6 @@ app.get('/books/:isbn', function(request, response) {
         response.send(body);
     });
 
-});
-
-
-app.get('*', (req, res) => {
-  match(
-    { routes, location: req.url },
-    (err, redirectLocation, renderProps) => {
-
-      if (err) {
-        return res.status(500).send(err.message);
-      }
-
-      let markup;
-      if (renderProps) {
-        markup = renderToString(<RouterContext {...renderProps}/>);
-      } else {
-        markup = renderToString(<NotFoundPage/>);
-        res.status(404);
-      }
-
-      return res.render('index', { markup });
-    }
-  );
 });
 
 app.listen(app.get('port'), function() {
