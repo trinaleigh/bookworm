b1a28359c628462c5c37b9316bf21694be73f202
trinaleigh <trinaleigh@users.noreply.github.com>
2017-02-15 15:06:25 -0500

    create user component

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,64 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router';
-import LibraryData from './LibraryData';
-import ISBNs from './ISBNs';
-import $ from 'jquery';
+import BookSelector from './BookSelector';
 
-export default class BookSelector extends React.Component {
+export default class Library extends React.Component {
 	
 	constructor(props) {
 		super(props);
-		this.state = {value: '', isbns: []};
-		this.handleChange = this.handleChange.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
-	}
-
-	componentDidMount() {
-  		function getList(userid){
-		    // access user's isbns from mongodb
-		    return $.ajax({
-		            url: `/user/${userid}`,
-		            dataType: 'json'        
-			})
-		}; 
-
-		function loadList(dblist){
-			this.setState({isbns: dblist});
-		}
-
-		getList("001").then(loadList.bind(this));
-
-	}
-
-
-	handleChange(event) {
-		// allow digits only for ISBN input
-		this.setState({value: event.target.value.replace(/\D/g,'')});
-	}
-
-	handleSubmit(event) {
-		// upon submission, add book to the list
-		this.setState({value: '', isbns : this.state.isbns.concat([this.state.value])})
-		event.preventDefault();
 	}
 
   	render() {
 
 	    return (
-			<div>
-
-				<form onSubmit={this.handleSubmit}>
-					<label>
-					  <h2>Add ISBN: </h2> 
-					  <input type="text" name="isbn" placeholder="9780486415918" 
-					  	value={this.state.value} onChange={this.handleChange} required/>
-					</label>
-					<input type="submit" value="Submit" />
-				</form>
-
-			  <ISBNs isbns={this.state.isbns}/>
-
-			</div>
+			<BookSelector userid="001"/>
 	    );
 	}
 }
