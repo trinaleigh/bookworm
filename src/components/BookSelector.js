f1d6712f5f9c54a64fc6949484ab5421eb1cc4a3
trinaleigh <trinaleigh@users.noreply.github.com>
2017-02-27 14:21:15 -0500

    add isbn validation

--- src/components/BookSelector.js
+++ src/components/BookSelector.js
@@ -8,7 +8,7 @@ export default class BookSelector extends React.Component {
 	
 	constructor(props) {
 		super(props);
-		this.state = {value: '', isbns: []};
+		this.state = {value: '', isbns: [], mode: 'valid'};
 		this.handleChange = this.handleChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
 		this.refreshData = this.refreshData.bind(this);
@@ -41,22 +41,29 @@ export default class BookSelector extends React.Component {
 
 	handleChange(event) {
 		// allow digits only for ISBN input
-		this.setState({value: event.target.value.replace(/\D/g,'')});
+		this.setState({value: event.target.value.replace(/\D/g,''), mode: 'valid'});
 	}
 
 	handleSubmit(event) {
 
-		function putList(userid, isbn){
-		    // access user's isbns from mongodb
-		    return $.ajax({
-		            url: `/upload/${userid}/${isbn}`,
-			})
-		}; 
+		if([10,13].includes(this.state.value.length)) {
 
-		putList(this.props.userid, this.state.value)
-		
-		// upon submission, add book to the list
-		this.setState({value: '', isbns : this.state.isbns.concat([this.state.value])})
+			function putList(userid, isbn){
+			    // access user's isbns from mongodb
+			    return $.ajax({
+			            url: `/upload/${userid}/${isbn}`,
+				})
+			}; 
+
+			putList(this.props.userid, this.state.value)
+			
+			// upon submission, add book to the list
+			this.setState({value: '', isbns : this.state.isbns.concat([this.state.value])})
+
+		} else {
+			this.setState({mode: 'invalid'})
+		}
+
 		event.preventDefault();
 	}
 
@@ -71,9 +78,10 @@ export default class BookSelector extends React.Component {
 					<label>
 					  <h2>Add ISBN: </h2> 
 					  <input type="text" name="isbn" placeholder="9780486415918" 
-					  	value={this.state.value} onChange={this.handleChange} required/>
+					  	value={this.state.value} onChange={this.handleChange}/>
 					</label>
 					<input type="submit" value="Submit" />
+					<span className={this.state.mode}>ISBN must be 10 or 13 digits long</span>
 				</form>
 
 			</div>
