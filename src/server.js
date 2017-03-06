afea68d0b880b4769bf8fe2d6fa7574fe4719dbf
trinaleigh <trinaleigh@users.noreply.github.com>
2017-03-06 13:39:03 -0500

    formatting

--- src/server.js
+++ src/server.js
@@ -22,174 +22,174 @@ function reduceString(s) { return (' ' + s).substr(1); }
 
 app.get('/staffpicks', function(request, response) {
 
-  var urls = ['http://www.greenlightbookstore.com/staffpicks',
-              'http://www.bookpeople.com/staff-picks', 
-              'http://www.inkwoodbooks.com/storepicks',
-              'http://www.tatteredcover.com/tc-staff-picks',
-              'http://www.elliottbaybook.com/',
-              'http://www.goldennotebook.com/staff-picks'];
+	var urls = ['http://www.greenlightbookstore.com/staffpicks',
+	          'http://www.bookpeople.com/staff-picks', 
+	          'http://www.inkwoodbooks.com/storepicks',
+	          'http://www.tatteredcover.com/tc-staff-picks',
+	          'http://www.elliottbaybook.com/',
+	          'http://www.goldennotebook.com/staff-picks'];
 
-  var n = Math.floor(Math.random()*urls.length);
-  var url = urls[n];
+	var n = Math.floor(Math.random()*urls.length);
+	var url = urls[n];
 
-  fetch(url)
-      .then(function(result){
-          return result.text();
-      }).then(function(body) {
+	fetch(url)
+		.then(function(result){
+			return result.text();
+		}).then(function(body) {
 
-          var $ = cheerio.load(body);
-          
-          // pick a random book from the list
-          var allBooks = $('.abaproduct-details');
-          var i = Math.floor(Math.random()*allBooks.length);
-          var choice = allBooks[i];
+			var $ = cheerio.load(body);
 
-          // get book details
-          var data = $(choice);
-          var title = reduceString(data.children('.abaproduct-title').text());
-          var author = reduceString(data.children('.abaproduct-authors').text().replace('By ',''));
-          var isbn = reduceString(data.children('.abaproduct-isbn').text().replace('ISBN: ',''));
+			// pick a random book from the list
+			var allBooks = $('.abaproduct-details');
+			var i = Math.floor(Math.random()*allBooks.length);
+			var choice = allBooks[i];
 
-          var source = reduceString($('title').text())
+			// get book details
+			var data = $(choice);
+			var title = reduceString(data.children('.abaproduct-title').text());
+			var author = reduceString(data.children('.abaproduct-authors').text().replace('By ',''));
+			var isbn = reduceString(data.children('.abaproduct-isbn').text().replace('ISBN: ',''));
 
-          var recommendation = { 
-              title, 
-              author, 
-              isbn, 
-              url, 
-              source};
+			var source = reduceString($('title').text())
 
-          // clear variables
-          $=null;
-          allBooks=null;
-          choice=null;
-          data=null;
-          title=null;
-          author=null;
-          isbn=null;
-          source=null
+			var recommendation = { 
+			  title, 
+			  author, 
+			  isbn, 
+			  url, 
+			  source};
 
-          // send to client
-          response.send(recommendation);
+			// clear variables
+			$=null;
+			allBooks=null;
+			choice=null;
+			data=null;
+			title=null;
+			author=null;
+			isbn=null;
+			source=null
 
-      })
+			// send to client
+			response.send(recommendation);
+
+		})
 
 });
 
 
 app.get('/bestsellers', function(request, response) {
 
-  var url = 'http://www.barnesandnoble.com/b/the-new-york-times-bestsellers/_/N-1p3n';
-  var source = 'New York Times Best Sellers'
+	var url = 'http://www.barnesandnoble.com/b/the-new-york-times-bestsellers/_/N-1p3n';
+	var source = 'New York Times Best Sellers'
 
-  fetch(url)
-      .then(function(result){
-          return result.text();
-      }).then(function(body) {
-            var $ = cheerio.load(body);
+	fetch(url)
+		.then(function(result){
+			return result.text();
+		}).then(function(body) {
+		    var $ = cheerio.load(body);
 
-            // get the bestseller selection
-            var book = $('.primary-content')
-              .children('#hotBooksWithDesc')
-              .first()
-              .children('#book-carousel')
-              .children('li').first();
-            
-            // get book details
-            var title = reduceString(book.children('p').text());
-            var author = reduceString(book.children('a').last().text());
-            var isbn = reduceString(book.children('a').first().attr('href')).split("ean=")[1];
+		    // get the bestseller selection
+		    var book = $('.primary-content')
+				.children('#hotBooksWithDesc')
+				.first()
+				.children('#book-carousel')
+				.children('li').first();
+		    
+		    // get book details
+		    var title = reduceString(book.children('p').text());
+		    var author = reduceString(book.children('a').last().text());
+		    var isbn = reduceString(book.children('a').first().attr('href')).split("ean=")[1];
 
-            var recommendation = { 
-                title, 
-                author, 
-                isbn, 
-                url, 
-                source};
+		    var recommendation = { 
+		        title, 
+		        author, 
+		        isbn, 
+		        url, 
+		        source};
 
-            // send to client
-            response.send(recommendation);
+		    // send to client
+		    response.send(recommendation);
 
-      })
+		})
 
 });
 
 app.get('/newsfeed', function(request, response) {
 
-  // check RSS feeds from book news sources
-  var urls = ['http://www.npr.org/rss/rss.php?id=1032',
-          'http://rss.nytimes.com/services/xml/rss/nyt/Books.xml',
-          'http://www.publishersweekly.com/pw/feeds/section/industry-news/index.xml',
-          'https://www.buzzfeed.com/books.xml',
-          'https://www.bookbrowse.com/rss/book_news.rss'];
+	// check RSS feeds from book news sources
+	var urls = ['http://www.npr.org/rss/rss.php?id=1032',
+	      'http://rss.nytimes.com/services/xml/rss/nyt/Books.xml',
+	      'http://www.publishersweekly.com/pw/feeds/section/industry-news/index.xml',
+	      'https://www.buzzfeed.com/books.xml',
+	      'https://www.bookbrowse.com/rss/book_news.rss'];
 
-  function getFeed(url) {
-      feedItem = fetch(url)
-          .then(function(result){
-              return result.text();
-          }).then(function(body) {
-                var $ = cheerio.load(body, {xmlMode: true});
+	function getFeed(url) {
+		feedItem = fetch(url)
+		  .then(function(result){
+		      return result.text();
+		  }).then(function(body) {
+		        var $ = cheerio.load(body, {xmlMode: true});
 
-                var source = $('title').first().text();
-                var sourceLink = $('link').first().text();
-                var story = $('item').first().children('title').text();
-                var storyLink = $('item').first().children('link').text();
-                
-                var feedItem = {
-                  source,
-                  sourceLink,
-                  story,
-                  storyLink };
+		        var source = $('title').first().text();
+		        var sourceLink = $('link').first().text();
+		        var story = $('item').first().children('title').text();
+		        var storyLink = $('item').first().children('link').text();
+		        
+		        var feedItem = {
+					source,
+					sourceLink,
+					story,
+					storyLink };
 
-                return feedItem;
-          })
+		        return feedItem;
+		  })
 
-      return new Promise(function(resolve, reject) {      
-          if (feedItem) {
-              resolve(feedItem);
-          } else {
-            reject(Error("newsfeed failed"));
-          }
+		return new Promise(function(resolve, reject) {      
+			if (feedItem) {
+				resolve(feedItem);
+			} else {
+				reject(Error("newsfeed failed"));
+			}
 
-        })
-      }
+		})
+	}
 
-  // send the parsed feed data
+	// send the parsed feed data
 
-  Promise.all(urls.map(getFeed)).then(function(value) {response.send(value)})
+	Promise.all(urls.map(getFeed)).then(function(value) {response.send(value)})
 
 });
 
 app.get('/books/:isbn', function(request, response) {
 
-  isbn = request.params.isbn;
+	isbn = request.params.isbn;
 
-  url = `http://lx2.loc.gov:210/lcdb?version=1.1&operation=searchRetrieve&query=bath.isbn=${isbn}&maximumRecords=1&recordSchema=mods`;
+	url = `http://lx2.loc.gov:210/lcdb?version=1.1&operation=searchRetrieve&query=bath.isbn=${isbn}&maximumRecords=1&recordSchema=mods`;
 
-  console.log('looking up book');
+	console.log('looking up book');
 
-  fetch(url)
-    .then(function(result) {
-        return result.text();
-    }).then(function(body) {
-        response.send(body);
-    });
+	fetch(url)
+	.then(function(result) {
+		return result.text();
+	}).then(function(body) {
+	    response.send(body);
+	});
 
 });
 
 app.get('/recs/:genre/:topic', function(request,response){
 
-  var genre = request.params.genre;
-  var topic = request.params.topic;
+	var genre = request.params.genre;
+	var topic = request.params.topic;
 
-  var url = `http://lx2.loc.gov:210/lcdb?version=1.1&operation=searchRetrieve&query=bath.subject=${genre}%20AND%20bath.subject=${topic}%20AND%20bath.any=text%20AND%20bath.any=eng&startRecord=1&maximumRecords=1&recordSchema=mods`;
+	var url = `http://lx2.loc.gov:210/lcdb?version=1.1&operation=searchRetrieve&query=bath.subject=${genre}%20AND%20bath.subject=${topic}%20AND%20bath.any=text%20AND%20bath.any=eng&startRecord=1&maximumRecords=1&recordSchema=mods`;
 
-  fetch(url)
-    .then(function(result) {
-        return result.text();
-    }).then(function(body) {
-        response.send(body);
-    });
+	fetch(url)
+	.then(function(result) {
+	    return result.text();
+	}).then(function(body) {
+	    response.send(body);
+	});
 
 });
 
@@ -218,92 +218,91 @@ app.get('/lists', function(request, response) {
 
 app.get('/user/:userid/:listype', function(request, response) {
 
-  var userid = request.params.userid;
-  var listype = request.params.listype;
+	var userid = request.params.userid;
+	var listype = request.params.listype;
 
-  MongoClient.connect(mongoUrl, function(err, db) {
-    assert.equal(null, err);
-    var findDocuments = function(db,callback) {
-      // Get the documents collection
-      var collection = db.collection('readers');
-      // Find some documents
-      collection.find({'userid': userid}).toArray(function(err, docs) {
-        assert.equal(err, null);
-        var userList = docs[0][listype];
-        callback(userList);
-      });   
-    }
+	MongoClient.connect(mongoUrl, function(err, db) {
+		assert.equal(null, err);
+		var findDocuments = function(db,callback) {
+			// Get the documents collection
+			var collection = db.collection('readers');
+			// Find some documents
+			collection.find({'userid': userid}).toArray(function(err, docs) {
+				assert.equal(err, null);
+				var userList = docs[0][listype];
+				callback(userList);
+		  	});   
+		}
 
-    findDocuments(db, function(userList){
-      response.send(userList)});
-
-    db.close();
-  });
+		findDocuments(db, function(userList){
+			response.send(userList)});
+			db.close();
+	});
 
 });
 
 app.post('/bookshelf/:userid', function(request, response, next) {
 
-  var userid = request.params.userid;
-  var book = request.body.book;
+	var userid = request.params.userid;
+	var book = request.body.book;
 
-  MongoClient.connect(mongoUrl, function(err, db) {
-    assert.equal(null, err);
+	MongoClient.connect(mongoUrl, function(err, db) {
+		assert.equal(null, err);
 
-    var updateDocument = function(db,callback) {
-      // Get the documents collection
-      var collection = db.collection('readers');
-      // Update document
-      collection.updateOne({ 'userid' : userid }
-        , { $push: { 'books' : book } }, function(err, result) {
-        assert.equal(err, null);
-        console.log("added book to shelf");
-        callback()
-      });  
-    }
+		var updateDocument = function(db,callback) {
+			// Get the documents collection
+			var collection = db.collection('readers');
+			// Update document
+			collection.updateOne({ 'userid' : userid }
+			, { $push: { 'books' : book } }, function(err, result) {
+				assert.equal(err, null);
+				console.log("added book to shelf");
+				callback()
+			});  
+		}
 
-    updateDocument(db, function(){
-      response.send("added book to shelf");
-      next();
-    })
+		updateDocument(db, function(){
+			response.send("added book to shelf");
+			next();
+		})
 
-    db.close();
-  });
+		db.close();
+	});
 
 });
 
 
 app.get('/remove/:userid/:isbn', function(request, response, next) {
 
-  var userid = request.params.userid;
-  var isbn = request.params.isbn;
+	var userid = request.params.userid;
+	var isbn = request.params.isbn;
 
-  MongoClient.connect(mongoUrl, function(err, db) {
-    assert.equal(null, err);
+	MongoClient.connect(mongoUrl, function(err, db) {
+		assert.equal(null, err);
 
-    var updateDocument = function(db,callback) {
-      // Get the documents collection
-      var collection = db.collection('readers');
-      // Update document
-      collection.updateOne({ 'userid' : userid }
-        , { $pull: { 'books' : {'isbn' : isbn} } }, function(err, result) {
-        assert.equal(err, null);
-        console.log("removed isbn");
-        callback()
-      });  
-    }
+		var updateDocument = function(db,callback) {
+			// Get the documents collection
+			var collection = db.collection('readers');
+			// Update document
+			collection.updateOne({ 'userid' : userid }
+			, { $pull: { 'books' : {'isbn' : isbn} } }, function(err, result) {
+				assert.equal(err, null);
+				console.log("removed isbn");
+				callback()
+			});  
+		}
 
-    updateDocument(db, function(){
-      response.send("removed isbn")
-      next();
-    })
+		updateDocument(db, function(){
+			response.send("removed isbn")
+			next();
+		})
 
-    db.close();
-  });
+		db.close();
+	});
 
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+	console.log('Node app is running on port', app.get('port'));
 });
 
