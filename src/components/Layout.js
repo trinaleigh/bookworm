c04c185ec2a3bbc6b6e278695cfe593075772781
trinaleigh <trinaleigh@users.noreply.github.com>
2017-03-07 10:46:01 -0500

    split components

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,119 +5,133 @@ import $ from 'jquery';
 
 export default class Layout extends React.Component {
   
-  constructor(props) {
+	constructor(props) {
+	    super(props);
+	    this.state = {allProfiles: [], user: 'classics', createMode: false, newList: '', mode: "valid"};
+	    this.refreshData = this.refreshData.bind(this);
+	    this.handleChange = this.handleChange.bind(this);
+	}
+  
+	handleChange(event) {
+		this.setState({user: event.target.value});
+	}
+
+	componentDidMount(){
+		this.refreshData();
+	}
+
+	refreshData(){
+		function getLists(){
+		    // access user's isbns from mongodb
+		    return $.ajax({
+		            url: `/lists`,
+		            dataType: 'json'        
+			})
+		}; 
+
+		function loadLists(dblist){
+			this.setState({allProfiles: dblist});  // load isbns and erase input box value
+		}
+
+		getLists().then(loadLists.bind(this));
+	}
+
+	render() {
+	    return (
+	      <div className="app">
+	        <header>
+	          <h1>hello, bookworm!</h1>
+	          <div id="login">
+	            <form>
+	              <label>
+	                Reading List:
+	                <select value={this.state.value} onChange={this.handleChange}>
+	                {this.state.allProfiles.map(profile => 
+	                	<option value={profile}>{profile}</option>)
+	                }
+	                </select>
+	              </label>
+	            <CreateModal handler={this.refreshData} allProfiles={this.state.allProfiles}/>  
+	            </form>
+	         </div>
+	        </header>
+	        <Navbar/>
+	        <div className="main-content">{React.cloneElement(this.props.children, { userid: this.state.user })}</div>
+	        <footer>
+	          <p>
+	            ...the end
+	          </p>
+	        </footer>
+	      </div>
+	    );
+	}
+}
+
+class CreateModal extends React.Component {
+
+    constructor(props) {
     super(props);
-    this.state = {allProfiles: [], user: 'classics', createMode: false, newList: '', mode: "valid"};
-    this.refreshData = this.refreshData.bind(this);
-    this.handleChange = this.handleChange.bind(this);
+    this.state = {createMode: false, newList: '', mode: "valid"};
     this.createModal = this.createModal.bind(this);
     this.handleEntry = this.handleEntry.bind(this);
     this.addList = this.addList.bind(this);
-  }
-  
-  handleChange(event) {
-  	this.setState({user: event.target.value});
-  }
-
-  createModal() {
-  	this.setState({createMode: !this.state.createMode});
-  }
-
-  handleEntry(event) {
-  	this.setState({newList: event.target.value, mode: "valid"});
-  }
-
-  addList(event) {
-
-    function sendList(listName){
-    // access Library of Congress online catalog
-        return $.ajax({
-                url: `/addlist/${listName}`,
-        })
-    };
-
-  	if (! this.state.allProfiles.includes(this.state.newList)){
-        sendList(this.state.newList)
-            .then(this.refreshData);
-        this.setState({createMode: !this.state.createMode, newList: ''});
-                
-  	} else {
-  		this.setState({mode: "invalid"});
-  	}
-  	event.preventDefault();
-  }
-
-  componentDidMount(){
-    this.refreshData();
-  }
-
-  refreshData(){
-  	    function getLists(){
-        // access user's isbns from mongodb
-        return $.ajax({
-                url: `/lists`,
-                dataType: 'json'        
-      })
-    }; 
-
-    function loadLists(dblist){
-    	this.setState({allProfiles: dblist});  // load isbns and erase input box value
-    }
-
-    getLists().then(loadLists.bind(this));
-  }
-
-
-
-  render() {
-
-  	let addList = null;
-	if (this.state.createMode == true ) {
-		addList = <div>
-					<div className="waiting"/>
-					<form className="list-modal" onSubmit={this.addList}>
-						<label>
-						  <h2>Create new list: </h2> 
-						  <input type="text" name="newList" placeholder="list_name" 
-						  	value={this.state.newList} onChange={this.handleEntry}/>
-						</label>
-						<input type="submit" value="Submit" />
-						<span className={this.state.mode}>List name already in use</span>
-					</form>
-				</div>;
-	} else {
-		addList = <div/>;
 	}
 
-    return (
-      <div className="app">
-        <header>
-          <h1>hello, bookworm!</h1>
-          <div id="login">
-            <form>
-              <label>
-                Reading List:
-                <select value={this.state.value} onChange={this.handleChange}>
-                {this.state.allProfiles.map(profile => 
-                	<option value={profile}>{profile}</option>)
-                }
-                </select>
-              </label>
-              <p className="mini-text" onClick={this.createModal}>+ create new list</p>
-              {addList}
-            </form>
-         </div>
-        </header>
-        <Navbar/>
-        <div className="main-content">{React.cloneElement(this.props.children, { userid: this.state.user })}</div>
-        <footer>
-          <p>
-            ...the end
-          </p>
-        </footer>
-      </div>
-    );
-  }
+	createModal() {
+  		this.setState({createMode: !this.state.createMode});
+  	}
+
+	handleEntry(event) {
+		this.setState({newList: event.target.value, mode: "valid"});
+	}
+
+	addList(event) {
+
+		function sendList(listName){
+		// access Library of Congress online catalog
+		    return $.ajax({
+		            url: `/addlist/${listName}`,
+		    })
+		};
+
+			if (! this.props.allProfiles.includes(this.state.newList)){
+		    sendList(this.state.newList)
+		        .then(this.props.handler);
+		    this.setState({createMode: !this.state.createMode, newList: ''});
+		            
+			} else {
+				this.setState({mode: "invalid"});
+			}
+			event.preventDefault();
+	}
+
+	render(){
+		let addList = null;
+		if (this.state.createMode == true ) {
+			addList = <div>
+						<div className="waiting"/>
+						<form className="list-modal" onSubmit={this.addList}>
+							<label>
+							  <h2>Create new list: </h2> 
+							  <input type="text" name="newList" placeholder="list_name" 
+							  	value={this.state.newList} onChange={this.handleEntry}/>
+							</label>
+							<input type="submit" value="Submit" />
+							<span className={this.state.mode}>List name already in use</span>
+						</form>
+					</div>;
+		} else {
+			addList = <div/>;
+		}
+
+		return (
+			<div>
+			<p className="mini-text" onClick={this.createModal}>+ create new list</p>
+			{addList}
+			</div>
+		)
+	}
+
 }
 
 class Navbar extends React.Component {
